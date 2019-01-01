fdee1f4e6e213c0ccd7ee9211c10429f81982d31
sbustos112 <46115298+sbustos112@users.noreply.github.com>
2019-01-01 13:07:07 -0800

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,1 +1,6 @@
-
+$ mkdir Anaconda && cd Anaconda
+$ mkdir game
+$ touch game/__init__.py
+$ virtualenv venv --python=python3.6 # Optional
+$ source venv/bin/activate # Optional
+$ pip install pygame
